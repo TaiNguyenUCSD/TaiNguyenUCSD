e17064bafb97f0ce546cc6f532d9c47b8ae6a127
Pootisplank <ialelin3401@gmail.com>
2021-02-20 13:25:31 -0800

    routing and express files

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+const profileRouter = require("./routes/profile");
+
+// Set static assets
+app.use(express.static("/public/assets"));
+app.use("/img", express.static(__dirname + "/img"));
+app.use("/styles", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", __dirname + "/public/layout");
+app.use(expressLayouts);
+
+// Routes
+app.use("/", indexRouter);
+app.use("/profile", profileRouter);
+
+const PORT = process.env.PORT || 8081;
