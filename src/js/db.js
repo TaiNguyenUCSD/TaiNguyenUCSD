c6a3652a1ede5b26d7ab0419ed914c97925c2cb2
Pootisplank <ialelin3401@gmail.com>
2021-02-20 18:29:43 -0800

    firebase

---
+++ src/js/db.js
@@ -0,0 +1,22 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+let firebaseConfig = {
+  apiKey: "AIzaSyAzmxB4QfJGPEciUXh5lf3Nr6xwOOl81dI",
+  authDomain: "suitefinder-cf0fe.firebaseapp.com",
+  projectId: "suitefinder-cf0fe",
+  storageBucket: "suitefinder-cf0fe.appspot.com",
+  messagingSenderId: "923975331245",
+  appId: "1:923975331245:web:0af52cc9310438e5158524",
+  measurementId: "G-RXRMNHCW4L",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
