fdf116daa5e66f5a73bac5d30625ed51f5b37c26
Tai <tan005@ucsd.edu>
2021-02-21 00:29:14 -0800

    Makes sense

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,6 @@
 let fname = document.getElementById("name");
 let submit = document.getElementById("submit-name");
 
-var database = firebase.database();
-function writeToFirebase(username) {
-  firebase.database().ref('users/' + username).set({
-    title: username
-  });
-}
-
-submit.addEventListener("click", fname);
-
+submit.addEventListener("click", function () {
+  console.log(fname.value);
+});
