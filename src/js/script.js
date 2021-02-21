a5c5af9f824eedd88ab3b64c42e4c0dc694439f4
oscarmtz1 <oscarmtz91702@gmail.com>
2021-02-21 01:11:56 -0800

    firebase implementation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,15 @@
 let fname = document.getElementById("name");
 let submit = document.getElementById("submit-name");
 
+var database = firebase.database();
+
+function writeUserData(fname){
+  firebase.database().ref('users/'+fname).set({
+    username: fname
+  });
+}
+
 submit.addEventListener("click", function () {
   console.log(fname.value);
 });
+
