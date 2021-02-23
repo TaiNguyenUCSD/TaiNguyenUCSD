5a06e5f763a17bde4c88475790ee65b641b89dd5
TaiNguyenUCSD <65136598+TaiNguyenUCSD@users.noreply.github.com>
2021-02-22 17:58:20 -0800

    Update handleStuff.js

--- handleStuff.js
+++ handleStuff.js
@@ -1,6 +1,14 @@
 let fname = document.getElementById("name");
 let submit = document.getElementById("submit");
 
+var database = firebase.database();
+
+function writeUserData(fname){
+  firebase.database().ref('users/'+fname).set({
+    username: fname
+  });
+}
+
 submit.addEventListener("click", function () {
   console.log(fname.value);
 });
