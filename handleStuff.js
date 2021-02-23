521424db02004b0fd2bf571e07d2b0af4abaa437
Tai <tan005@ucsd.edu>
2021-02-22 18:30:57 -0800

    bro

--- handleStuff.js
+++ handleStuff.js
@@ -1,14 +1,6 @@
 let fname = document.getElementById("name");
 let submit = document.getElementById("submit");
 
-var database = firebase.database();
 
-function writeUserData(fname){
-  firebase.database().ref('users/'+fname).set({
-    username: fname
-  });
-}
 
-submit.addEventListener("click", function () {
-  console.log(fname.value);
-});
+submit.addEventListener("click",console.log(fname.value), false); 
